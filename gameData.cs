a3bd3d9af3f261f366f0a1f92d2941fd330b8519
Edward Kozlowski <ekozlowski1@gmail.com>
2015-08-30 21:27:13 -0500

    Latest

---
+++ gameData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdsACPlugin
+{
+    class GameData
+    {
+        // This class should only ever be updated when the 
+        // game's data changes.
+        
+
+        private void LoadData()
+        {
+            Console.WriteLine("foo");
+        }
+
+        public GameData()
+        {
+            LoadData();
+        }
+    }
+}
+ 
